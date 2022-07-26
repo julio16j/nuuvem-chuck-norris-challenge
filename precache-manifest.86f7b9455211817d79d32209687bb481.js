78df5edb12556b7b987a750e0dc4ee67ff6e80b6
Julio Cesar Urbano <jc.urbano.vasconcelos@gmail.com>
2022-07-26 06:15:13 -0300

    Updates

---
+++ precache-manifest.86f7b9455211817d79d32209687bb481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "079c665c6040b652d164",
+    "url": "/nuuvem-chuck-norris-challenge/static/css/main.ae2d36e2.chunk.css"
+  },
+  {
+    "revision": "079c665c6040b652d164",
+    "url": "/nuuvem-chuck-norris-challenge/static/js/main.079c665c.chunk.js"
+  },
+  {
+    "revision": "6336b426631f80f6c7a2",
+    "url": "/nuuvem-chuck-norris-challenge/static/js/1.6336b426.chunk.js"
+  },
+  {
+    "revision": "9f3c71781cb4204f6d9f",
+    "url": "/nuuvem-chuck-norris-challenge/static/js/2.9f3c7178.chunk.js"
+  },
+  {
+    "revision": "1c9b9453631e7543229f",
+    "url": "/nuuvem-chuck-norris-challenge/static/js/runtime~main.1c9b9453.js"
+  },
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/nuuvem-chuck-norris-challenge/static/media/chucknorris_logo_coloured.39b94c0e.png"
+  },
+  {
+    "revision": "b0f80f3ebb7b82313e67296c5d20d1a1",
+    "url": "/nuuvem-chuck-norris-challenge/index.html"
+  }
+];
