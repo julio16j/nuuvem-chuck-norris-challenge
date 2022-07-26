3db8a7c15d6e06c660432da0de4e8a04252cdcb2
Julio Cesar Urbano <jc.urbano.vasconcelos@gmail.com>
2022-07-26 06:44:43 -0300

    corrigindo erros de routeamento do github pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import HomePage from './pages/Home'
-import NotFound from './pages/NotFound'
 import SearchJokesPage from './pages/SearchJokes'
 import React from 'react';
 import Helmet from 'react-helmet'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 function App() {
   return (
     <div>
@@ -11,13 +10,13 @@ function App() {
         <title>Chuck Norris Jokes</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
       </Helmet>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/jokes" element={<SearchJokesPage />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<HomePage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossOrigin="anonymous"/>
     </div>
   );
