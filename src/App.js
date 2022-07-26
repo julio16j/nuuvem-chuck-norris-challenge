e25df415abbddda4d5858b4658a85e77a24da301
Julio Cesar Urbano <jc.urbano.vasconcelos@gmail.com>
2022-07-26 17:16:52 -0300

    adaptacoes IOS

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     <div>
       <Helmet>
         <title>Chuck Norris Jokes</title>
+        <meta name="theme-color" content="#282C34" media="(prefers-color-scheme: light)" /> 
+        <meta name="theme-color" content="#282C34" media="(prefers-color-scheme: dark)" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
       </Helmet>
       <HashRouter>
